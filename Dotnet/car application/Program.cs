0229ec13ae9dd96b27bcb8b0a65de8dffba81878
Kami Yoba <kami.yoba10@gmail.com>
2023-04-26 13:13:27 +0300

    update car

--- Dotnet/car application/Program.cs
+++ Dotnet/car application/Program.cs
@@ -1,44 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// variable declaration: <type> <name> = <value>;
+string name = "John Doe";
 
-class Car {
-    public string Model { get; set; }
-    public string Make { get; set; }
-    public int CurrentSpeed { get; private set; }
-    public bool EngineRunning { get; private set; }
+Car myCar = new();
+myCar.Make = "Toyota";
 
-    public void StartEngine() {
-        if (!EngineRunning) {
-            Console.WriteLine("Starting engine...");
-            EngineRunning = true;
-        }
-    }
 
-    public void StopEngine() {
-        if (EngineRunning) {
-            Console.WriteLine("Stopping engine...");
-            EngineRunning = false;
-            CurrentSpeed = 0;
-        }
-    }
 
-    public void Accelerate(int speed) {
-        if (EngineRunning) {
-            Console.WriteLine("Accelerating to {0} mph...", speed);
-            CurrentSpeed = speed;
-        }
-        else {
-            Console.WriteLine("Cannot accelerate without the engine running!");
-        }
-    }
 
-    public void Decelerate(int speed) {
-        if (EngineRunning) {
-            Console.WriteLine("Decelerating to {0} mph...", speed);
-            CurrentSpeed = speed;
-        }
-        else {
-            Console.WriteLine("Cannot decelerate without the engine running!");
-        }
-    }
-}
+Car myCousinsCar = new();
+Car myFriendsCar = new();
+
