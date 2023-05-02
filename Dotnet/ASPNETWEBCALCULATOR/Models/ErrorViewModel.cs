d7a2bd7d991434f51112f3b9111b771e9108a3ab
Kami Yoba <kami.yoba10gmail.com>
2023-05-02 13:10:52 +0300

    Started developing an ASP.NET MVC calculator

---
+++ Dotnet/ASPNETWEBCALCULATOR/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ASPNETWEBCALCULATOR.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
